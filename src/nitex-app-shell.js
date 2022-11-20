723ed3229a2c33dfaf01b15f754cef6502717aff
Kumar Nitesh <contact@kumar-nitesh.com>
2022-11-20 16:12:07 -0500

    setting rxjs

--- src/nitex-app-shell.js
+++ src/nitex-app-shell.js
@@ -1,4 +1,18 @@
-import { registerApplication, start, pathToActiveWhen } from "single-spa";
+import {
+  registerApplication,
+  start,
+  pathToActiveWhen,
+  navigateToUrl,
+} from "single-spa";
+import { auth$ } from "@nitex/utility";
+
+let authenticated = false;
+
+const ROUTES = {
+  ROOT: "/",
+  NAVBAR: "/navbar",
+  DASHBOARD: "/dashboard",
+};
 
 registerApplication({
   name: "@nitex/app-nav-bar",
@@ -25,6 +39,23 @@ registerApplication({
   app: () => System.import("@nitex/app-dashboard"),
   activeWhen: activeWhen,
   customProps: { domElement: document.getElementById("main-container") },
+});
+
+activeWhen = pathToActiveWhen("/account", true);
+
+registerApplication({
+  name: "@nitex/app-account-detail",
+  app: () => System.import("@nitex/app-account-detail"),
+  activeWhen: activeWhen,
+  customProps: { domElement: document.getElementById("main-container") },
+});
+
+auth$.subscribe(({ sessionToken }) => {
+  authenticated = !!sessionToken;
+  if (!authenticated && window.location.pathname !== ROUTES.NAVBAR)
+    navigateToUrl(ROUTES.ROOT);
+  if (authenticated && window.location.pathname === ROUTES.NAVBAR)
+    navigateToUrl(ROUTES.DASHBOARD);
 });
 
 start({
